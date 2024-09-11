2508f8377e111f8bd0c7cb3c5fe809183d104ce7
Christina Bürger <christina.buerger@outlook.de>
2024-09-11 17:00:30 +0200

    add js folder and start with challange

---
+++ javascript-challenges/conditions-booleans-statements/js-conditions-and-booleans_statements/js/index.js
@@ -0,0 +1,43 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+const receivedPassword = "password1234";
+
+if (SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
+/* if (receivedPassword) {
+  console.log("Access denied!");
+} else {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} */
+
+// Part 2: Even / Odd
+const number = 8;
+
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
+// Part 3: Hotdogs
+const numberOfHotdogs = 42;
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement = "";
+
+console.log(statement);
+
+// Part 5: Greeting
+const userName = "Archibald";
+
+const greeting = "Hello " + "//enter your code here" + "!";
+
+console.log(greeting);
