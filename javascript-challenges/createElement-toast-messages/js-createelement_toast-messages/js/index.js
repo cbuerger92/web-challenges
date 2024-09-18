ab9384052fd700bb52c894e887c9d3f27bdc4b7a
Christina Bürger <christina.buerger@outlook.de>
2024-09-18 09:40:12 +0200

    toast challenge

---
+++ javascript-challenges/createElement-toast-messages/js-createelement_toast-messages/js/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+const toastContainer = document.querySelector('[data-js="toast-container"]');
+const addButton = document.querySelector('[data-js="add-button"]');
+const clearButton = document.querySelector('[data-js="clear-button"]');
+
+addButton.addEventListener("click", () => {
+  // Exercise: Append a new entry to the toast messages container
+  const newToastMessage = document.createElement("li"); // neue li Element
+
+  newToastMessage.className = "toast-container__message"; // Klasse hinzufügen
+
+  newToastMessage.textContent = "I'm a new toast message."; // Textinhalt
+
+  toastContainer.appendChild(newToastMessage); // Element zum Container hinzufügen
+});
+
+clearButton.addEventListener("click", () => {
+  // Exercise: Clear the stack of toast messages
+  while (toastContainer.firstChild) {
+    // überprüft, ob noch Kinder im Container vorhanden sind.
+    toastContainer.removeChild(toastContainer.firstChild); // entfernt das erste Kind-Element des Containers, bis der Container leer ist.
+  }
+});
