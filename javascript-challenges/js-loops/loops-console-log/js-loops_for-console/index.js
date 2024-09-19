f442c55105c2909fdaa511360ba4a4c4efb1c6fc
Christina Bürger <christina.buerger@outlook.de>
2024-09-19 11:23:47 +0200

    loops

---
+++ javascript-challenges/js-loops/loops-console-log/js-loops_for-console/index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+for (let i = 0; i <= 100; i++) {
+  console.log(i);
+}
+
+/* Interne Zählvariable (i): Die Variable i wird als Zähler verwendet und startet bei 0.
+
+Kriterium: Der Loop läuft, solange i kleiner oder gleich 100 ist (i <= 100).
+
+Regel zur Erhöhung: Die Zählvariable i wird nach jedem Durchlauf um 1 erhöht (i++). */
